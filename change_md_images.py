9a245e2c3850e137ea160c2616a71d3a7938df00
Shorya Malani <github-sho@smlist.eml.cc>
2020-05-06 14:55:17 -0400

    adding code

--- change_md_images.py
+++ change_md_images.py
@@ -7,30 +7,22 @@ from markdownify import markdownify as md
 from github import Github
 from git import Repo
 
-def download_image(image,github):
+def download_image(image,g):
     r = requests.get(image["src"], stream=True)
     periods = image["src"].count(".")
     print("/Users/shorya/Dropbox/programming_shorya/python_code/md-to-img"+image["src"].replace("/","_").replace(":","_").replace(".","_",periods-1))
     with open("/Users/shorya/Dropbox/programming_shorya/python_code/md-to-img"+image["src"].replace("/","_").replace(":","_").replace(".","_",periods-1), 'wb+') as f:
         f.write(r.content)
-    repo_dir = 'md-to-img'
-    repo = Repo(repo_dir)
-    file_list = [
-    "/Users/shorya/Dropbox/programming_shorya/python_code/md-to-img"+image["src"].replace("/","_").replace(":","_").replace(".","_",periods-1)
-    ]
-    commit_message = 'Add simple regression analysis'
-    repo.index.add(file_list)
-    repo.index.commit(commit_message)
-    origin = repo.remote('origin')
-    origin.push()
-    return "/Users/shorya/Dropbox/programming_shorya/python_code/md-to-img"+image["src"].replace("/","_").replace(":","_").replace(".","_",periods-1)
+    repo = g.get_user().get_repo("images_for_md")
+    repo.create_file("images/"+image["src"].replace("/","_").replace(":","_").replace(".","_",periods-1),"img",r.content)
+    return "https://github.com/juno-day/images-for-md"+"images/"+image["src"].replace("/","_").replace(":","_").replace(".","_",periods-1)
 
 def find_and_change_images(markdown_file):
     html = BeautifulSoup(markdown(markdown_file), "html.parser")
     images = html.find_all("img")
     print(images)
     number=0
-    g = Github("63b4e41dee5ad3735ffa34faba078b9c41854f6b")
+    g = Github("9df519d4d46d250515014157cc7d0668857c42c8")#9df519d4d46d250515014157cc7d0668857c42c8
     for image in images:
         new_image = download_image(image,g)
         html.find_all("img")[number]["src"] = new_image
@@ -42,7 +34,8 @@ def open_file_to_html(file):
         markdown_from_file = f.read()
         return markdown_from_file
 
-if
+if __name__ == "__main__":
+    main(sys.argv[1])
     
 # 63b4e41dee5ad3735ffa34faba078b9c41854f6b
 print(sys.argv)
