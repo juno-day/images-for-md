917c4bf7309067bc1b158304f90f3d73b9dec7b2
Shorya Malani <github-sho@smlist.eml.cc>
2020-05-06 16:09:17 -0400

    adding fixed token

--- change_md_images.py
+++ change_md_images.py
@@ -7,21 +7,24 @@ from markdownify import markdownify as md
 from github import Github
 from datetime import datetime
 from git import Repo
+import json
 def download_image(image,g):
     r = requests.get(image["src"], stream=True)
     periods = image["src"].count(".")
     print("images/"+image["src"].replace("/","_").replace(":","_").replace(".","_",periods-1))
     repo = g.get_user().get_repo("images-for-md")
-    remote_file_name = "images/"+image["src"].split("/")[-1] + datetime.now().strftime("%c%f").replace(" ","_") # have the image
+    remote_file_name = "images/"+ datetime.now().strftime("%c%f").replace(" ","_").replace(":","_")+image["src"].split("/")[-1]  # have the image
     repo.create_file(remote_file_name,"jpg",r.content,branch="master")
-    return "https://github.com/juno-day/images-for-md/"+"images/"+image["src"].replace("/","_").replace(":","_").replace(".","_",periods-1)
+    return "https://github.com/juno-day/images-for-md/"+remote_file_name
 
 def find_and_change_images(markdown_file):
     html = BeautifulSoup(markdown(markdown_file), "html.parser")
     images = html.find_all("img")
     print(images)
     number=0
-    g = Github("788938ee835cabe66124c7e362d01bd67865c95d") #9df519d4d46d250515014157cc7d0668857c42c8
+    with open("../gitcreds.json","r") as f:
+        data = json.load(f)
+    g = Github(data["git_cred"]) #9df519d4d46d250515014157cc7d0668857c42c8
     for image in images:
         new_image = download_image(image,g)
         html.find_all("img")[number]["src"] = new_image
@@ -43,6 +46,6 @@ except:
 markdowntxt = open_file_to_html(file)
 new_html_file = find_and_change_images(markdowntxt)
 print(new_html_file.find_all("img"))
-with open("/Users/shorya/Dropbox/programming_shorya/python_code/md-to-imgoutput_"+sys.argv[1],"w+") as f:
+with open(sys.argv[1],"w+") as f:
     print(md(new_html_file))
     f.write(md(new_html_file))
